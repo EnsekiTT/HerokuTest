6bf7fdd9e2f796248b64915f4626cba5aabee89a
EnsekiTT <ensekitt@gmail.com>
2017-03-15 21:45:09 +0900

    fix server.py

--- server.py
+++ server.py
@@ -1,6 +1,9 @@
 import tornado.ioloop
 import tornado.web
 
+from tornado.options import define
+define("port", default=5000, help="run on the given port", type=int)
+
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.write("Hello, world")
@@ -12,5 +15,5 @@ def make_app():
 
 if __name__ == "__main__":
     app = make_app()
-    app.listen(5000)
+    app.listen(tornado.options.options.port)
     tornado.ioloop.IOLoop.current().start()
