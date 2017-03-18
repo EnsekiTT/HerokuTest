672a1468750f7c2b6327e8bb0dcf2df98919f616
EnsekiTT <ensekitt@gmail.com>
2017-03-18 16:12:53 +0900

    fix Port setting

---
+++ README.md
@@ -0,0 +1,2 @@
+# HerokuTest for Python3 + Tornado
+[![Deploy](https://www.herokucdn.com/deploy/button.png)](https://heroku.com/deploy)
--- server.py
+++ server.py
@@ -2,12 +2,14 @@ import tornado.ioloop
 import tornado.web
 
 from tornado.options import define, options
+
 define("port", default=5000, help="run on the given port", type=int)
+define("user", default="hogeman", help="Set User Name", type=str)
 tornado.options.parse_command_line()
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        self.write("Hello, world")
+        self.write("Hello, " + options.user)
 
 def make_app():
     return tornado.web.Application([
@@ -16,5 +18,5 @@ def make_app():
 
 if __name__ == "__main__":
     app = make_app()
-    app.listen(tornado.options.options.port)
+    app.listen(options.port)
     tornado.ioloop.IOLoop.current().start()
